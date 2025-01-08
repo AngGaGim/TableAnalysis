9db0631173b97a57673c757bd9e921ce189af1e5
hongjiayin <1244417130@qq.com>
2025-01-08 16:24:14 +0800

    feat: 添加表格读取方法

--- services/table_async_service.py
+++ services/table_async_service.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from datetime import datetime
 
 
 class TableAsyncService:
@@ -29,4 +29,17 @@ class TableAsyncService:
 
     @staticmethod
     def _read_excel_to_df(file_path,sheet_name) -> pd.DataFrame:
-        origin_df = pd.read_excel(file_path,nrows=10,header=None,sheet_name=sheet_name)
+        '''
+        读取指定sheet的原始文件，保留前10行数据，表头为空
+        并对日期数据进行转换
+        :param file_path:
+        :param sheet_name:
+        :return:
+        '''
+        origin_df = pd.read_excel(file_path, nrows=10, header=None, sheet_name=sheet_name)
+        origin_df = origin_df.where(origin_df.notnull(), None).map(
+            lambda cell: (
+                cell.strftime('%Y-%m-%d') if isinstance(cell, (pd.Timestamp, datetime)) and pd.notnull(cell) else cell
+            )
+        )
+        return origin_df
